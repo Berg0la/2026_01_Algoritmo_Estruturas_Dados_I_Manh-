8e466f0a78d18378b09989a9e7e9a156beaf62fb
ROBERT MARK  LUCENA <01245529358@senacrs.edu.br>
2026-03-18 11:38:48 -0300

    Aula 01 - Lista Encadeada - continuação

--- A1-ListaEncadeada/Lista.py
+++ A1-ListaEncadeada/Lista.py
@@ -15,6 +15,7 @@ class Lista:
             while aux:
                 print( aux.dado )
                 aux = aux.prox
+        print("---------------")
                 
     def add(self, valor):
         nodo = No(valor)
@@ -26,3 +27,29 @@ class Lista:
                 aux = aux.prox
             aux.prox = nodo
         self.imprimir()
+        
+    def remover(self, valor):
+        if self.inicio is None:
+            print("Lista Vazia!")
+        else:
+            removeu = False
+            if valor == self.inicio.dado:
+                self.inicio = self.inicio.prox
+                removeu = True
+            else:
+                ant = self.inicio
+                aux = self.inicio.prox
+                while aux:
+                    if valor == aux.dado:
+                        ant.prox = aux.prox
+                        removeu = True
+                        break
+                    else:
+                        ant = aux
+                        aux = aux.prox
+                if removeu:
+                    print(valor, "removido!")
+                else:
+                    print(valor, "não encontrado!")
+                
+                self.imprimir()
