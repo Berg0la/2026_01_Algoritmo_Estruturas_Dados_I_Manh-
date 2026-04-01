15c1ad4226159273a0a33ed1c9c39299cea5ec2a
ROBERT MARK  LUCENA <01245529358@senacrs.edu.br>
2026-04-01 11:42:44 -0300

    Fila - Erro ao executar, será corrigido

---
+++ aula03 - Fila - Fifo/main.py
@@ -0,0 +1,14 @@
+from Fila import Fila
+
+fila = Fila()
+
+fila.imprimir()
+
+fila.add("Robert")
+fila.add("ZX4R")
+fila.add("DUCATI PANIGALI")
+fila.remover()
+fila.add("CBR1000RRR")
+fila.remover()
+fila.remover()
+fila.remover()
