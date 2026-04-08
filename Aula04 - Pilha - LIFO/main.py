3952fe9752a8c4cf2b2e805e39a290481461ee73
ROBERT MARK  LUCENA <01245529358@senacrs.edu.br>
2026-04-08 11:40:01 -0300

    Aula 4 - Pilha LIFO

---
+++ Aula04 - Pilha - LIFO/main.py
@@ -0,0 +1,13 @@
+from Pilha import Pilha
+
+pilha = Pilha()
+pilha.imprimir()
+
+pilha.add( "João" )
+pilha.add( "Maria" )
+pilha.add( "José" )
+pilha.remover()
+pilha.add( "Júlia" )
+pilha.remover()
+pilha.remover()
+pilha.remover()
