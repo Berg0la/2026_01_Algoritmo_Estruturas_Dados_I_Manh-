e59487ed90cc4d7b167ca26e2465f4b51201c153
ROBERT MARK  LUCENA <01245529358@senacrs.edu.br>
2026-04-15 11:36:10 -0300

    Exercicio livro - pilha - fifo

---
+++ Aula07-Livro/Pilha.py
@@ -0,0 +1,44 @@
+from Livro import Livro
+
+class Pilha:
+
+    def __init__(self):
+        self.topo = None
+
+    def add(self, livro):
+        if self.topo is not None: 
+            livro.prox = self.topo
+        self.topo = livro
+        self.imprimir()
+
+    def remover(self):
+        if self.topo is not None:
+            self.topo = self.topo.prox
+        self.imprimir()
+
+    def imprimir(self):
+        print("--------------------")
+        if self.topo is None:
+            print("\nPilha Vazia")
+        else:
+            print("\nPilha - LIFO")
+            aux = self.topo
+            while aux:
+                print( aux )
+                aux = aux.prox
+        print("--------------------")
+
+    def contLivrosPorAutor(self, nome):
+        if self.topo is not None:
+            cont = 0
+            aux = self.topo
+            while aux:
+                if nome == aux.autor.getNome():
+                    cont += 1
+                aux = aux.prox
+            if cont == 0 :
+                print(nome, "Não possui livros na pilha!")
+            else:
+                print(nome, "possui", cont, "livros na pilha")
+    
+    
